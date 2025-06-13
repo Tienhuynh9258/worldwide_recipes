06a7adf57c8b38035c44c48a5fdfd3ad92e3eb89
htn <huynhtien9258@gmail.com>
2025-06-14 00:06:04 +0700

    change port to 3000

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9002',
+    baseUrl: 'http://localhost:3000',
     supportFile: false,
   },
 }); 
